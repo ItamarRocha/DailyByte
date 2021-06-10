ea0674c5d861a759f040ac7ea2141e166e9737c1
itamar <itamardprf@gmail.com>
2021-06-10 11:44:26 -0300

    constant sapce

--- week_5/day35_findmode.py
+++ week_5/day35_findmode.py
@@ -77,3 +77,57 @@ root.right.right = BST(9)
 root.right.right.right = BST(9)
 
 print(findmode(root))
+
+print("\n\nSolution2:\n")
+# Solution 2
+# Time O(n) n -> number of nodes
+# Space O(1)
+def findmode(root):
+    if not root:
+        return -1
+
+    els = {"current_count" : float("-inf"),
+            "current_el" : None,
+            "max_count" : float("-inf"),
+            "max_el" : None}
+
+    dfs(root,els)
+
+    return els["max_el"]
+
+def dfs(root, els):
+    if not root:
+        return
+    
+    dfs(root.left, els)
+
+    if root.value == els["current_el"]:
+        els["current_count"] += 1
+    else:
+        els["current_el"] = root.value
+        els["current_count"] = 1
+    
+    if els["current_count"] > els["max_count"]:
+        els["max_count"] = els["current_count"]
+        els["max_el"] = els["current_el"]
+
+    dfs(root.right, els)
+
+    return
+
+root = BST(2)
+root.left = BST(1)
+root.right = BST(2)
+
+print(findmode(root))
+
+root = BST(7)
+root.left = BST(4)
+root.left.left = BST(1)
+root.left.right = BST(4)
+root.right = BST(9)
+root.right.left = BST(8)
+root.right.right = BST(9)
+root.right.right.right = BST(9)
+
+print(findmode(root))
