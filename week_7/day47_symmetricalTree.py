2a69e6f6e9c5ee9799dc42801bb4842287ba3df8
ItamarRocha <itamardprf@gmail.com>
2021-07-02 20:54:36 -0300

    day 47

--- week_7/day47_symmetricalTree.py
+++ week_7/day47_symmetricalTree.py
@@ -23,5 +23,96 @@ class Tree():
         self.left = None
         self.right = None
 
+# Time O(n)
+# Space O(2n)
+def bfs(root, left=True):
+    output = []
+    queue = [(0,0,root)]
+    while queue:
+        depth, horiz, node = queue.pop(0)
+
+        if not node:
+            continue
+
+        if left:
+            queue.append((depth + 1, horiz + 1, node.left))
+            queue.append((depth + 1, horiz - 1, node.right))
+        else:
+            queue.append((depth + 1, horiz + 1, node.right))
+            queue.append((depth + 1, horiz - 1, node.left))
+
+
+        output.append((depth, horiz, node.value))
+    
+    return output
+
 def symmetricaltree(root):
-    pass
+    output1 = bfs(root.left, left=True)
+    output2 = bfs(root.right, left=False)
+
+    print(output1 == output2)
+
+root = Tree(2)
+root.left = Tree(1)
+root.right = Tree(1)
+
+symmetricaltree(root)
+
+root = Tree(1)
+root.left = Tree(5)
+root.right = Tree(5)
+root.left.right = Tree(7)
+root.right.right = Tree(7)
+
+symmetricaltree(root)
+
+root = Tree(1)
+root.left = Tree(2)
+root.right = Tree(2)
+root.left.right = Tree(4)
+root.left.left = Tree(3)
+root.right.left = Tree(4)
+root.right.right = Tree(3)
+
+symmetricaltree(root)
+
+# time O(n)
+# Space O(n)
+def symmetricaltree(root):
+    if not root:
+        return True
+    stack = [(root.left, root.right)]
+    while stack:
+        l, r = stack.pop()
+        if not l and not r:
+            continue
+        if not l or not r or (l.value != r.value):
+            return False
+        stack.append((l.left, r.right))
+        stack.append((l.right, r.left))
+    return True
+
+print("solution 2:\n")
+root = Tree(2)
+root.left = Tree(1)
+root.right = Tree(1)
+
+print(symmetricaltree(root))
+
+root = Tree(1)
+root.left = Tree(5)
+root.right = Tree(5)
+root.left.right = Tree(7)
+root.right.right = Tree(7)
+
+print(symmetricaltree(root))
+
+root = Tree(1)
+root.left = Tree(2)
+root.right = Tree(2)
+root.left.right = Tree(4)
+root.left.left = Tree(3)
+root.right.left = Tree(4)
+root.right.right = Tree(3)
+
+print(symmetricaltree(root))
