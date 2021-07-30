89672942621d724775c98d0896ccf1bb8e7c1eb4
itamar <itamardprf@gmail.com>
2021-07-30 12:37:54 -0300

    day84

--- week_12/day84_countingprimes.py
+++ week_12/day84_countingprimes.py
@@ -17,16 +17,14 @@ def countprimes(n):
     if n <= 2:
         return 0
     primes = [True] * (n)
-    primes[0] = False
-    primes[1] = False
+    primes[0] = primes[1] = False
 
     for i in range(2,int(n ** 0.5) + 1):
         if not primes[i]:
             continue
 
         for j in range(i+i,n, i):
-            if primes[j]:
-                primes[j] = False
+            primes[j] = False
 
     return sum(primes)
 
